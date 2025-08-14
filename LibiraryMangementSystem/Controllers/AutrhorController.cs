a58882c65848b03fac87b2ee9a11e4f95303d9a6
mr-70da <man3092003city@gmail.com>
2025-08-14 13:53:35 +0300

    Refactored: seprate LibiraryController to two diffrent controllers.

---
+++ LibiraryMangementSystem/Controllers/AutrhorController.cs
@@ -0,0 +1,68 @@
+﻿using LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AutrhorController : ControllerBase
+    {
+        private LibraryContext _libraryContext;
+        public AutrhorController(LibraryContext context)
+        {
+            _libraryContext = context;
+        }
+
+        //Create new auther
+        [HttpPost()]
+        public IActionResult Create([FromBody] Author newAuthor)
+        {
+            if (newAuthor == null)
+            {
+                return BadRequest("Invalid author data.");
+            }
+            if (_libraryContext.Authors.Any(a => a.FirstName == newAuthor.FirstName && a.LastName == newAuthor.LastName))
+            {
+                return Conflict("Author already exists.");
+            }
+            _libraryContext.Authors.Add(newAuthor);
+            _libraryContext.SaveChanges();
+            return Ok("Created!");
+        }
+        [HttpDelete()]
+        public IActionResult Delete(int authorId)
+        {
+            var author = _libraryContext.Authors.Find(authorId);
+            if (author == null)
+            {
+                return NotFound("Author not found.");
+            }
+            if (_libraryContext.Books.Any(aob => aob.Authors.Any(a => a.Id == authorId)))
+            {
+                return BadRequest("Cannot delete author with associated books.");
+            }
+            _libraryContext.Authors.Remove(author);
+            _libraryContext.SaveChanges();
+            return Ok("Author deleted successfully.");
+        }
+
+        [HttpPut()]
+        public IActionResult Update(int authorId, [FromBody] Author updatedAuthor)
+        {
+            if (updatedAuthor == null)
+            {
+                return BadRequest("Invalid author data.");
+            }
+            var existingAuthor = _libraryContext.Authors.Find(authorId);
+            if (existingAuthor == null)
+            {
+                return NotFound("Author not found.");
+            }
+            existingAuthor.FirstName = updatedAuthor.FirstName;
+            existingAuthor.LastName = updatedAuthor.LastName;
+            _libraryContext.SaveChanges();
+            return Ok(existingAuthor);
+        }
+    }
+}
---
+++ LibiraryMangementSystem/Controllers/BookController.cs
@@ -0,0 +1,82 @@
+﻿using LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        LibraryContext _libraryContext;
+        public BookController(LibraryContext context)
+        {
+            _libraryContext = context;
+        }
+
+        [HttpGet("byAuther/{authorId}")]
+        public IActionResult GetAll(int authorId)
+        {
+            var booksByAuthor = _libraryContext.Books
+                               .Where(b => b.Authors.Any(a => a.Id == authorId))
+                               .Select(b => new
+                               {
+                                   b.Isbn,
+                                   b.Title,
+                                   b.Edition,
+                                   b.Price,
+
+                               })
+                               .ToList();
+
+            var result = new
+            {
+                AuthorId = authorId,
+                TotalBooks = booksByAuthor.Count,
+                Books = booksByAuthor
+            };
+
+            return Ok(result);
+        }
+        //create new book with author
+        [HttpPost("{authorId}")]
+        public IActionResult Create(int authorId, [FromBody] Book newBook)
+        {
+            if (newBook == null)
+                return BadRequest("Invalid book data.");
+
+
+            Author author = _libraryContext.Authors.Find(authorId);
+
+            if (author == null)
+                return BadRequest("Author not found.");
+
+            author.BookIsbns.Add(newBook);
+
+            _libraryContext.SaveChanges();
+
+            return Ok("Created!");
+        }
+        [HttpPut("{bookIsbn}")]
+        public IActionResult Update(int bookIsbn, [FromBody] Book updatedBook)
+        {
+            if (updatedBook == null)
+            {
+                return BadRequest("Invalid Book data.");
+            }
+            Book existingBook = _libraryContext.Books.Find(bookIsbn);
+            if (existingBook == null)
+            {
+                return BadRequest("Book not found!");
+            }
+            existingBook.Title = updatedBook.Title;
+            existingBook.Price = updatedBook.Price;
+            existingBook.CopyRightYear = updatedBook.CopyRightYear;
+            existingBook.Edition = updatedBook.Edition;
+            _libraryContext.SaveChanges();
+            return Ok("Updated!");
+
+        }
+
+    }
+}
